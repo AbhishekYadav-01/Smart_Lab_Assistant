70ae26569a9a80518ec0baa904afe1231c06b8b1
AbhishekYadav-01 <b22es020@iitj.ac.in>
2025-10-05 23:29:18 +0530

    Timezone issue resolved.

--- simulation.py
+++ simulation.py
@@ -1,7 +1,7 @@
 # simulation.py
 import asyncio
 import json
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import fastapi 
 import sqlalchemy 
 from auth import User 
@@ -20,14 +20,14 @@ class MultiAgentTrafficSystem:
         self.agent_map: Dict[str, LabAgent] = {}
 
     async def broadcast_schedule_update(self):
-        today = datetime.now()
+        today = datetime.now(timezone.utc)
         start_of_week = today - timedelta(days=today.weekday())
         end_of_week = start_of_week + timedelta(days=7)
         schedule_data = await self.get_schedule_for_range(start_of_week, end_of_week)
         await self.manager.broadcast(json.dumps({"type": "schedule_update", "data": schedule_data}))
 
     async def get_full_schedule(self) -> dict:
-            today = datetime.now()
+            today = datetime.now(timezone.utc)
             start_of_week = today - timedelta(days=today.weekday())
             end_of_week = start_of_week + timedelta(days=7)
             return await self.get_schedule_for_range(start_of_week, end_of_week)
